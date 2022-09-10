e41289737c1ab97b66a86fc07f8454ad239ccc10
jguillermo222 <ja.guillermo222@gmail.com>
2022-09-10 13:06:33 -0500

    Version 3.0

---
+++ src/main/java/com/empresa2/Empresa2022Application.java
@@ -0,0 +1,14 @@
+package com.empresa2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Empresa2022Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Empresa2022Application.class, args);
+	}
+
+
+}
